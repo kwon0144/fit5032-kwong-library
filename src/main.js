92f28cdf116c1787b788241bafd149f1f8d85d3b
kwon0144 <kwon0144@student.monash.edu>
2024-10-08 04:06:52 +1100

    Clear Cache and Reinstall Dependencies

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 
 //import './assets/main.css'
 //import './style.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
