c6414fab858daf506d7a811cfd2aed094faf0d59
kwon0144 <kwon0144@student.monash.edu>
2024-10-17 22:30:23 +1100

    set up cd pipeline

---
+++ week11/kwong-library-deploy/src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      // your routes here  
+    }
+  ]
+})
+
+export default router
+
